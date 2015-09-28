3233679929c00285f5ca19f03eef065208a40e58
ericguti.90 <ericguti.90@gmail.com>
2015-09-28 12:41:21 +0200

    news activities

--- app/src/main/java/es/cat/cofb/bbsaccess/Presentation/DetalleActivity.java
+++ app/src/main/java/es/cat/cofb/bbsaccess/Presentation/DetalleActivity.java
@@ -13,6 +13,6 @@ public class DetalleActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_detalle);
+		setContentView(R.layout.activity_detalle_evento);
 	}
 }
