3065a710701dfe8451e6f1e64bd6306b2a85b5d5
ericguti.90 <ericguti.90@gmail.com>
2015-11-04 13:34:50 +0100

    menu lateral

--- app/src/main/java/es/cat/cofb/bbsaccess/Listeners/MenuListener.java
+++ app/src/main/java/es/cat/cofb/bbsaccess/Listeners/MenuListener.java
@@ -1,11 +1,15 @@
 package es.cat.cofb.bbsaccess.Listeners;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import es.cat.cofb.bbsaccess.Presentation.MainActivity;
 import es.cat.cofb.bbsaccess.R;
 
 /**
@@ -25,22 +29,32 @@ public class MenuListener implements NavigationView.OnNavigationItemSelectedList
     public boolean onNavigationItemSelected(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.avis_legal:
-                Toast.makeText(applicationContext, "avis legal", Toast.LENGTH_SHORT).show();
+                Toast.makeText(applicationContext, "proximament", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.bustia:
-
+                Toast.makeText(applicationContext,"proximament", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.contacte:
-
+                Toast.makeText(applicationContext,"proximament", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.idioma:
-
+                Toast.makeText(applicationContext,"proximament", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.perfil:
-                Toast.makeText(applicationContext,"perfil", Toast.LENGTH_SHORT).show();
+                Toast.makeText(applicationContext,"proximament", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.sessio:
-
+                String user = "user";
+                String pass = "pass";
+                SharedPreferences prefs = applicationContext.getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.remove(user);
+                editor.remove(pass);
+                editor.apply();
+                Intent intent = new Intent(applicationContext, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                applicationContext.startActivity(intent);
+                ((Activity)applicationContext).finish();
                 break;
         }
         drawerLayout.closeDrawers();
